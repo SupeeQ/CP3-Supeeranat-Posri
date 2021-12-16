4c9cee4f397953832e108db8a5400875196ee418
SupeeQ <96105693+SupeeQ@users.noreply.github.com>
2021-12-16 10:56:49 +0700

    Create Exercise4_Supeeranat_P.py
    First Excercise

---
+++ Exercise4_Supeeranat_P.py
@@ -0,0 +1,10 @@
+Sc_Eng = 90.0
+Sc_GB = 70.5
+Sc_Comsys = 80.4
+Sc_Comprog = 100.0
+print("--- Your Score ---------------------------------")
+print("Foundation English               : ",Sc_Eng)
+print("General Business                 : ",Sc_GB)
+print("Introduction to Computer Systems : ",Sc_Comsys)
+print("Computer Programming             : ",Sc_Comprog)
+print("------------------------------------------------")
